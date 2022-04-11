d2d451a6f6f0ccad81d059fc728f4f36de74c0b7
Hardeep Chhabra <hardeep96singh@gmail.com>
2022-04-11 15:30:42 +0530

    RegisterScreen White Mode Started but not yet Completed

---
+++ White Mode CSS/cssRegisterScreen.js
@@ -0,0 +1,363 @@
+import { StyleSheet } from "react-native";
+
+
+
+
+const styles = StyleSheet.create({
+    SignUp: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      position: "relative",
+      paddingTop: 0,
+      paddingBottom: 35,
+      paddingLeft: 37,
+      paddingRight: 0,
+      backgroundColor: "white",
+      width: 375,
+      height: 667,
+      flex:1
+    },
+  
+    Group696: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    Txt698: {
+      fontSize: 16,
+      fontFamily: "undefined",
+      fontWeight: "600",
+      color: "rgba(255, 255, 255, 1)",
+      marginBottom: 84,
+    },
+  
+    Group2611: {
+      // display: "flex",
+      // flexDirection: "row",
+      // paddingTop: 14,
+      // paddingBottom: 14,
+      // paddingLeft: 13,
+      // paddingRight: 192,
+      marginBottom: 28,
+      borderRadius: 5,
+      backgroundColor: "rgba(229,229,229,1)",
+      shadowColor: "rgba(0,97,255,1)",
+      elevation: 1,
+      shadowOffset: { width: 0, height: 2 },
+    },
+  
+    userIcon:{
+      top: 12,
+      left: 10,
+      position: "absolute",
+      // color: "black",
+      // fontSize: 30,
+      width: 20,
+      height: 20
+    },
+  
+    hidePassword: {
+      top: 12,
+      right: 10,
+      position: "absolute",
+      // color: "black",
+      // fontSize: 30,
+      width: 20,
+      height: 20,
+    },
+  
+    User: {
+      width: 20,
+      height: 20,
+      marginRight: 10,
+    },
+    Txt342: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    Mail: {
+      width: 20,
+      height: 20,
+      marginRight: 11,
+    },
+    Txt342: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    Mail: {
+      width: 20,
+      height: 20,
+      marginRight: 11,
+    },
+    Txt342: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    User: {
+      width: 20,
+      height: 20,
+      marginRight: 10,
+    },
+    Txt743: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+      marginRight: 160,
+    },
+    Hide: {
+      width: 20,
+      height: 20,
+    },
+  
+    Line1: {
+      borderWidth: 0.75,
+      borderStyle: "solid",
+      borderColor: "rgba(255, 255, 255, 1)",
+      width: 285,
+      height: 0.75,
+      marginBottom: 30,
+    },
+  
+  multiple1: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "500",
+      color: "rgba(255, 255, 255, 1)",
+      marginBottom: 31,
+    },
+  
+    multiple2: {
+      fontSize: 12,
+      fontFamily: "undefined",
+      fontWeight: "500",
+      color: "rgba(0,97,255,1)",
+      marginBottom: 31,
+    },
+  
+    Group: {
+      position: "absolute",
+      top: 133,
+      left: 203,
+      width: 321.86,
+      height: 287.76,
+    },
+  
+    Group457: {
+      paddingTop: 14,
+      paddingBottom: 12,
+      paddingLeft: 120,
+      paddingRight: 100,
+      marginBottom: 34,
+      marginTop: 44,
+      borderRadius: 5,
+      backgroundColor: "rgba(0,97,255,1)",
+      width: 300,
+      height: 50,
+    },
+  
+    Txt4104: {
+      fontSize: 14,
+      fontFamily: "undefined",
+      fontWeight: "600",
+      color: "rgba(255, 255, 255, 1)",
+      // width: 56,
+    },
+  
+  })
+
+
+  export default styles;
+
+
+  const styles11 = StyleSheet.create({
+    SignUp: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      position: "relative",
+      paddingTop: 0,
+      paddingBottom: 35,
+      paddingLeft: 37,
+      paddingRight: 0,
+      backgroundColor: "white",
+      /*  linear-gradient(-90deg, rgba(255, 255, 255, 0.9) 0%, rgba(241,240,241,0.9) 100%, ) , linear-gradient(0deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 1)) */
+      width: 375,
+      height: 667,
+    },
+    Group654: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    Txt524: {
+      fontSize: 16,
+      fontFamily: "Raleway, sans-serif",
+      fontWeight: "600",
+      color: "rgba(55,49,64,1)",
+      marginBottom: 84,
+    },
+    Group4104: {
+      display: "flex",
+      flexDirection: "row",
+      paddingTop: 14,
+      paddingBottom: 14,
+      paddingLeft: 11,
+      paddingRight: 194,
+      marginBottom: 16,
+      borderRadius: 5,
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      shadowColor: "rgba(0,97,255,1)",
+      elevation: 1,
+      shadowOffset: { width: 0, height: 2 },
+    },
+    User: {
+      width: 20,
+      height: 20,
+      marginRight: 10,
+    },
+    Txt500: {
+      fontSize: 12,
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    Group275: {
+      display: "flex",
+      flexDirection: "row",
+      paddingTop: 15,
+      paddingBottom: 13,
+      paddingLeft: 11,
+      paddingRight: 208,
+      marginBottom: 16,
+      borderRadius: 5,
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      shadowColor: "rgba(0,97,255,1)",
+      elevation: 1,
+      shadowOffset: { width: 0, height: 2 },
+    },
+    Mail: {
+      width: 20,
+      height: 20,
+      marginRight: 11,
+    },
+    Txt500: {
+      fontSize: 12,
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    Group238: {
+      display: "flex",
+      flexDirection: "row",
+      paddingTop: 15,
+      paddingBottom: 13,
+      paddingLeft: 11,
+      paddingRight: 163,
+      marginBottom: 16,
+      borderRadius: 5,
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      shadowColor: "rgba(0,97,255,1)",
+      elevation: 1,
+      shadowOffset: { width: 0, height: 2 },
+    },
+    Mail: {
+      width: 20,
+      height: 20,
+      marginRight: 11,
+    },
+    Txt500: {
+      fontSize: 12,
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+    },
+  
+    Group468: {
+      display: "flex",
+      flexDirection: "row",
+      paddingTop: 15,
+      paddingBottom: 13,
+      paddingLeft: 12,
+      paddingRight: 17,
+      marginBottom: 42,
+      borderRadius: 5,
+      backgroundColor: "rgba(255, 255, 255, 1)",
+      shadowColor: "rgba(0,97,255,1)",
+      elevation: 1,
+      shadowOffset: { width: 0, height: 2 },
+    },
+    User: {
+      width: 20,
+      height: 20,
+      marginRight: 10,
+    },
+    Txt738: {
+      fontSize: 12,
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "400",
+      color: "rgba(0,0,0,1)",
+      marginRight: 160,
+    },
+    Hide: {
+      width: 20,
+      height: 20,
+    },
+  
+    Group297: {
+      paddingTop: 15,
+      paddingBottom: 14,
+      paddingLeft: 118,
+      paddingRight: 117,
+      marginBottom: 45,
+      borderRadius: 5,
+      backgroundColor: "rgba(0,97,255,1)",
+      width: 300,
+      height: 50,
+    },
+    Txt696: {
+      fontSize: 14,
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "600",
+      color: "rgba(255, 255, 255, 1)",
+      width: 63,
+      height: 19,
+    },
+  
+    Line1: {
+      borderWidth: 0.75,
+      borderStyle: "solid",
+      borderColor: "rgba(55,49,64,1)",
+      width: 285,
+      height: 0.75,
+      marginBottom: 30,
+    },
+    multiple1: {
+      main: "Txt645",
+      seg1: "[object Object]",
+      seg2: "[object Object]",
+      seg3: "[object Object]",
+    },
+  
+    Group: {
+      position: "absolute",
+      top: 133,
+      left: 203,
+      width: 321.86,
+      height: 287.76,
+    },
+  })
